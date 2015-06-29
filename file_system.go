8e5a61851b959cfdf22ad163b97f0235ea2b1b1d
Daniel Martinez <eduardodaniel2@gmail.com>
2015-06-29 12:11:01 -0500

    Adds file system functions to perform directory listing

---
+++ file_system.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	l4g "code.google.com/p/log4go"
+	"path/filepath"
+)
+
+const (
+	FILE_TYPE = "*.xml"
+)
+
+func ListFiles(globPattern string) (matches []string, err error) {
+	return filepath.Glob(globPattern)
+}
+
+func GetDirectoriesList(options map[string]interface{}) (output []string) {
+	baseDir := options["<path_location>"].(string)
+	rfcList, _ := getPatternList(baseDir)
+
+	for _, dir := range rfcList {
+		output = append(output, filepath.Join(dir, FILE_TYPE))
+		l4g.Debug(filepath.Join(dir, FILE_TYPE))
+	}
+	return
+}
+
+func getPatternList(baseDir string) (matches []string, err error) {
+	return filepath.Glob(filepath.Join(baseDir))
+}
